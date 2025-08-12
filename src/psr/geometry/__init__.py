46abf20ea8188abbda0c205de9bcd94d8fda54d1
deepdyn <168887587+deepdyn@users.noreply.github.com>
2025-08-12 10:10:58 +0530

    Create __init__.py

---
+++ src/psr/geometry/__init__.py
@@ -0,0 +1,12 @@
+from .poincare import PoincarePlane, plane_from_point_normal
+from .sections import SectionEnsemble, SectionHit
+from .return_maps import return_map_pairs, polar_coords_on_plane
+
+__all__ = [
+    "PoincarePlane",
+    "plane_from_point_normal",
+    "SectionEnsemble",
+    "SectionHit",
+    "return_map_pairs",
+    "polar_coords_on_plane",
+]
